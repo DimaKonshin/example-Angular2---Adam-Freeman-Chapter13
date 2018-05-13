6137cf9715b86713bf707f9f50dc6a2947a34385
Dima <some@mail>
2018-05-13 16:15:22 +0400

    0 start

---
+++ example/app/component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
+
+@Component({
+    selector: "app",
+    templateUrl: "app/template.html"
+})
+export class ProductComponent {
+    
+}
